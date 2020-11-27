e859bfc539bd58814ade25c230918405433086a3
Simon Ingeson <44818+smonn@users.noreply.github.com>
2020-11-27 12:51:01 -0500

    Reduce packaged bundle size

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import bundleSize from 'rollup-plugin-bundle-size';
-import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
@@ -7,33 +6,13 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.main.replace('.js', '.min.js'),
-      format: 'cjs',
-      plugins: [terser()],
-    },
-    {
       file: pkg.module,
       format: 'es',
     },
     {
-      file: pkg.module.replace('.js', '.min.js'),
-      format: 'es',
-      plugins: [terser()],
-    },
-    {
-      file: pkg.browser,
+      file: pkg.main,
       format: 'umd',
       name: 'ValidatorFns',
-    },
-    {
-      file: pkg.browser.replace('.js', '.min.js'),
-      format: 'umd',
-      name: 'ValidatorFns',
-      plugins: [terser()],
     },
   ],
   plugins: [bundleSize(), typescript({})],
