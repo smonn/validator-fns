f817ad94241916a3014632eefd933fb396839e2b
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-05-26 11:07:03 -0400

    Switch back to c8 for code coverage

--- test/objects.test.ts
+++ test/objects.test.ts
@@ -1,4 +1,4 @@
-import test from 'ava';
+import test from "ava";
 import {
 	integer,
 	max,
@@ -7,146 +7,149 @@ import {
 	object,
 	required,
 	string,
-	ValidatorTest
-} from '../src/index.js';
+	ValidatorTest,
+} from "../src/index.js";
 
-test('object', async t => {
+test("object", async (t) => {
 	const validate = object({
 		username: string(
-			required('Username is required.'),
-			min(5, 'At least five characters.'),
-			max(20, 'At most 20 characters.')
+			required("Username is required."),
+			min(5, "At least five characters."),
+			max(20, "At most 20 characters.")
 		),
 		age: number(
-			required('Must enter your age.'),
-			min(18, 'At least 18 years old'),
-			max(150, 'Are you really over 150 years old?'),
-			integer('Half-years do not count.')
-		)
+			required("Must enter your age."),
+			min(18, "At least 18 years old"),
+			max(150, "Are you really over 150 years old?"),
+			integer("Half-years do not count.")
+		),
 	});
 
 	t.deepEqual(
 		await validate({
-			username: 'hello',
-			age: '20'
+			username: "hello",
+			age: "20",
 		}),
 		{
-			state: 'valid',
+			state: "valid",
 			value: {
-				username: 'hello',
-				age: 20
+				username: "hello",
+				age: 20,
 			},
 			isValid: true,
-			field: undefined
+			field: undefined,
 		}
 	);
 
 	t.deepEqual(
 		await validate({
-			username: 'hello',
-			age: null
+			username: "hello",
+			age: null,
 		}),
 		{
-			state: 'invalid',
+			state: "invalid",
 			isValid: false,
 			field: undefined,
-			message: '',
+			message: "",
 			value: {
-				username: 'hello',
-				age: null
+				username: "hello",
+				age: null,
 			},
 			errors: {
-				age: 'Must enter your age.'
-			}
+				age: "Must enter your age.",
+			},
 		}
 	);
 });
 
-test('empty object config is always valid', async t => {
+test("empty object config is always valid", async (t) => {
 	const validate = object({});
 
-	t.deepEqual(await validate({foo: 'bar'}), {
-		state: 'valid',
+	t.deepEqual(await validate({ foo: "bar" }), {
+		state: "valid",
 		value: {},
 		isValid: true,
-		field: undefined
+		field: undefined,
 	});
 	t.deepEqual(await validate(null), {
-		state: 'valid',
+		state: "valid",
 		value: {},
 		isValid: true,
-		field: undefined
+		field: undefined,
 	});
 	t.deepEqual(await validate(undefined), {
-		state: 'valid',
+		state: "valid",
 		value: {},
 		isValid: true,
-		field: undefined
+		field: undefined,
 	});
 });
 
-test('nested object', async t => {
+test("nested object", async (t) => {
 	// While this is technically possible, it's not recommended usage as it quickly gets quite complex
 	const validate = object({
 		person: object({
-			firstName: string(required('required')),
-			lastName: string(required('required')),
-			age: number()
-		})
+			firstName: string(required("required")),
+			lastName: string(required("required")),
+			age: number(),
+		}),
 	});
 
 	t.deepEqual(
 		await validate({
 			person: {
-				firstName: 'foo',
-				lastName: 'bar'
-			}
+				firstName: "foo",
+				lastName: "bar",
+			},
 		}),
 		{
-			state: 'valid',
+			state: "valid",
 			value: {
 				person: {
-					firstName: 'foo',
-					lastName: 'bar',
-					age: undefined
-				}
+					firstName: "foo",
+					lastName: "bar",
+					age: undefined,
+				},
 			},
 			isValid: true,
-			field: undefined
+			field: undefined,
 		}
 	);
 
 	t.deepEqual(
 		await validate({
 			person: {
-				firstName: 'foo',
-				age: '22'
-			}
+				firstName: "foo",
+				age: "22",
+			},
 		}),
 		{
-			state: 'invalid',
-			message: '',
+			state: "invalid",
+			message: "",
 			value: {
 				person: {
-					firstName: 'foo',
-					age: '22'
-				}
+					firstName: "foo",
+					age: "22",
+				},
 			},
 			errors: {
 				person: {
-					lastName: 'required'
-				}
+					lastName: "required",
+				},
 			},
 			isValid: false,
-			field: undefined
+			field: undefined,
 		}
 	);
 });
 
-test('invalid configuration', t => {
-	const config: Record<string, ValidatorTest> = 'bad value' as any as Record<
-	string,
-	ValidatorTest
+test("invalid configuration", async (t) => {
+	const config: Record<string, ValidatorTest> = "bad value" as any as Record<
+		string,
+		ValidatorTest
 	>;
 	t.throws(() => object(config));
+
+	const validate = object({ test: "test" } as any);
+	await validate(null);
 });
