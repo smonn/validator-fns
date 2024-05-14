743631f0979f15d23f189e194e6415a784faef45
Simon Ingeson <github@smonn.se>
2024-05-14 11:02:30 -0400

    add lcov reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'lcov'],
       exclude: ['runkit.js', 'benchmark/**', ...coverageConfigDefaults.exclude],
     },
   },
